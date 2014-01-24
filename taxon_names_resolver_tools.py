1d2a9fdffe43620f71d2f2d22d64da368618ac97
Dom Bennett <dominic.john.bennett@gmail.com>
2014-01-24 16:28:33 +0000

    Adapted the genGenTax function to output qnames on tips

--- taxon_names_resolver_tools.py
+++ taxon_names_resolver_tools.py
@@ -6,30 +6,33 @@
 ## TODO: write doctests
 ## TODO: test mergeLineage and extractHighestClade with duplicated test data
 
-import collections
+import collections, re
 from Bio import Phylo
 from cStringIO import StringIO
 
-def genTaxTree(resolver, by_ids = False, draw = False):
+def genTaxTree(resolver, by = 'qnames', draw = False):
 	"""Generate Newick tree from TaxonNamesResolver class.
         
         Arguments:
          resolver = TaxonNamesResolver class
-         by_ids = Use taxon IDs instead of names (logical)
+         by = Tip labels, either 'qnames', 'taxids' or 'name_string'
          draw = Draw ascii tree (logical)
 
          Return:
           (Newick Tree Object, [shared lineage])"""
-        if by_ids:
-            idents = resolver.retrieve('taxon_id')
-	    idents = ["tx{0}".format(e) for e in idents]
-	    print idents
-            lineages = resolver.retrieve('classification_path_ids')
-            ranks = resolver.retrieve('classification_path_ranks')
-        else:
-            idents = resolver.retrieve('name_string')
-            lineages = resolver.retrieve('classification_path')
-            ranks = resolver.retrieve('classification_path_ranks')
+	ranks = resolver.retrieve('classification_path_ranks')
+        if by == 'qnames' or by == 'name_string':
+		lineages = resolver.retrieve('classification_path')
+		if by == 'qnames':
+			idents = resolver.retrieve('query_name')
+		else:
+			idents = resolver.retrieve('name_string')
+	elif by == 'taxids':
+		idents = resolver.retrieve('taxon_id')
+		idents = ["tx{0}".format(e) for e in idents]
+		lineages = resolver.retrieve('classification_path_ids')          
+	else:
+		raise Error("Unrecognised 'by' argument (either qnames, taxids or name_string)")
 	for i, lineage in enumerate(lineages):
 		lineage.reverse()
 		lineages[i] = lineage
@@ -37,37 +40,38 @@ def genTaxTree(resolver, by_ids = False, draw = False):
 		rank.reverse()
 		ranks[i] = rank
 	# make lineages of same ranks
-	all_ranks = [e2 for e1 in ranks for e2 in e1]
-	rank_freq =  collections.Counter(all_ranks).items()
-	shared_ranks = [e for e, f in rank_freq if f == len(idents)]
-	line_bool = [[1 if e2 in shared_ranks else 0 for e2 in e1] for e1 in ranks]
-	lineages = [[lineages[i1][i2] for i2, e2 in enumerate(e1) if e2 == 1] for i1, e1 in enumerate(line_bool)]
-	all_lines = [e2 for e1 in lineages for e2 in e1]
-	line_freq =  collections.Counter(all_lines).items()
-	shared_lineage = [e for e, f in line_freq if f == len(idents)]
-	# TODO: if shared lineage is empty... drop radically different taxa
+        all_ranks = [e2 for e1 in ranks for e2 in e1]
+        rank_freq =  collections.Counter(all_ranks).items()
+        shared_ranks = [e for e, f in rank_freq if f == len(idents)]
+        line_bool = [[1 if e2 in shared_ranks else 0 for e2 in e1] for e1 in ranks]
+        lineages = [[lineages[i1][i2] for i2, e2 in enumerate(e1) if e2 == 1] for \
+                        i1, e1 in enumerate(line_bool)]
+        all_lines = [e2 for e1 in lineages for e2 in e1]
+        line_freq =  collections.Counter(all_lines).items()
+        shared_lineage = [e for e, f in line_freq if f == len(idents)]
 	# create line_obj, a tuple of ident and lineage
 	line_obj = zip(idents, lineages)
 	for i in range(len(lineages[0])):
-		for uniq in set([each[1][i] for each in line_obj]):
-			# find shared taxonomic groups
-			new_node = [each[0] for each in line_obj if each[1][i] == uniq]
-			if len(new_node) > 1:
-				# extract shared lineage
-				lineage = [each[1] for each in line_obj if each[0] == new_node[0]]
-				# remove shareds from line_obj
-				line_obj = [each for each in line_obj if not each[0] in new_node]
-				# convert to strings
-				new_node = [str(each) for each in new_node]
-				# add new node to line_obj
-				new_node = ('(' + ','.join(new_node) + ')', lineage[0])
-				line_obj.append(new_node)
-		if len(line_obj) < 1:
-			break
+            for uniq in set([each[1][i] for each in line_obj]):
+                # find shared taxonomic groups
+                new_node = [each[0] for each in line_obj if each[1][i] == uniq]
+                if len(new_node) > 1:
+                    # extract shared lineage
+                    lineage = [each[1] for each in line_obj if each[0] == new_node[0]]
+                    # remove shareds from line_obj
+                    line_obj = [each for each in line_obj if not each[0] in new_node]
+                    # convert to strings
+                    new_node = [str(each) for each in new_node]
+                    new_node = [re.sub("\\s", "_", each) for each in new_node]
+                    # add new node to line_obj
+                    new_node = ('(' + ','.join(new_node) + ')' + str(uniq), lineage[0])
+                    line_obj.append(new_node)
+            if len(line_obj) < 1:
+                break
         tree = Phylo.read(StringIO(line_obj[0][0] + ';'), "newick")
         if draw:
             Phylo.draw_ascii(tree)
-	return (tree, shared_lineage)
+	return tree, shared_lineage
 
 def lineageMerge(resolver, min_rank = 'genus'):
     """Return TaxonNamesResolver class with duplicated lineages merged
