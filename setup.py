012345a8dd7088d444add9b71a891386a3645c48
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-06-01 13:11:12 +0100

    Add arg parser and logging

--- setup.py
+++ setup.py
@@ -1,35 +1,42 @@
 #! /usr/bin/env python
+## D.J. Bennett
+## 01/06/2014
+"""
+TaxonNamesResolver setup
+"""
+
 import os
 from setuptools import setup, find_packages
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 PACKAGES = find_packages()
 PACKAGES = [each for each in PACKAGES if each != 'tests']
 PACKAGE_DIRS = [p.replace(".", os.path.sep) for p in PACKAGES]
 
 setup(
-    name = "taxon_names_resolver",
-    version = "0.0.1",
-    author = "Dominic John Bennett",
-    author_email = "dominic.john.bennett@gmail.com",
-    description = ("Resolve taxonomic names through Global Names Resolver."),
-    license = "No license",
-    keywords = "taxonomy biodiversity systematics natural history",
-    url = "https://github.com/DomBennett/TaxonNamesResolver",
-    packages = PACKAGES,
-    package_dir = dict(zip (PACKAGES, PACKAGE_DIRS)),
-    test_suite = 'tests',
-    scripts = ['TaxonNamesResolver.py'],
-    long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "Programming Language :: Python :: 2.7",
-    ],
-    install_requires=[
-          # -*- Extra requirements: -*-
-          'setuptools',
-      ],
+	name = "taxon_names_resolver",
+	version = "0.0.1",
+	author = "Dominic John Bennett",
+	author_email = "dominic.john.bennett@gmail.com",
+	description = ("Resolve taxonomic names through Global Names Resolver."),
+	license = "LICENSE.txt",
+	keywords = "taxonomy biodiversity systematics natural history",
+	url = "https://github.com/DomBennett/TaxonNamesResolver",
+	packages = PACKAGES,
+	package_dir = dict(zip (PACKAGES, PACKAGE_DIRS)),
+	test_suite = 'tests',
+	scripts = ['TaxonNamesResolver.py'],
+	long_description=read('README.md'),
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Topic :: Scientific/Engineering :: Bio-Informatics",
+		"Programming Language :: Python :: 2.7",
+		"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+	],
+	install_requires=[
+		  # -*- Extra requirements: -*-
+		  'setuptools',
+	  ],
 )
