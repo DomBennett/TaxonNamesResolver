046f95dacbc3333b44c143e1f0e0c516d041b165
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-12-03 21:56:42 +0000

    Minor edit

--- taxon_names_resolver/__init__.py
+++ taxon_names_resolver/__init__.py
@@ -22,5 +22,6 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
-# Load Resolver into namespace
+# Create namespace
 from taxon_names_resolver.resolver import Resolver
+from taxon_names_resolver.misc_tools import taxTree
