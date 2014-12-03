bbcbfdf98f885f19266d53a651a467d9b131dacb
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-12-03 21:56:32 +0000

    Minor edit

--- explore.py
+++ explore.py
@@ -6,7 +6,14 @@ console.setFormatter(logging.Formatter('%(message)s'))
 logger.addHandler(console)
 
 from taxon_names_resolver import Resolver
-#terms = ['Pt. niger', 'Leis','Harp', 'Not. biguttatus', 'Not. palustris', 'Amara', 'Cal. erratus', 'Pt. diligens', 'Not. reitteri']
-terms = ['Harp']
-resolver = Resolver(terms = terms, datasource = "NCBI")
+terms = ['Homo sapiens', 'Gorilla gorilla', 'Pongo pongo', 'Macca mulatta',
+         'Mus musculus', 'Ailuropoda melanoleuca', 'Ailurus fulgens',
+         'Chlorotalpa tytonis', 'Arabidopsis thaliana', 'Bacillus subtilus']
+resolver = Resolver(terms=terms, datasource="NCBI")
 resolver.main()
+
+from taxon_names_resolver import taxTree
+ranks = resolver.retrieve('classification_path_ranks')
+idents = resolver.retrieve('query_name')
+lineages = resolver.retrieve('classification_path')
+taxTree(idents, ranks, lineages, draw=True)
