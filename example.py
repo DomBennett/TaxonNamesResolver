14e43809687df7059a5ff09d22a229b81ea8db0a
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-12-05 16:27:08 +0000

    Renamed explore

--- example.py
+++ example.py
@@ -1,3 +1,9 @@
+#! /usr/bin/env python
+'''
+Example script for using taxon_names_resolver
+'''
+
+# SETUP LOGGING
 import logging
 logger = logging.getLogger('')
 logger.setLevel(logging.DEBUG)
@@ -5,15 +11,25 @@ console = logging.StreamHandler()
 console.setFormatter(logging.Formatter('%(message)s'))
 logger.addHandler(console)
 
+# PACKAGES
 from taxon_names_resolver import Resolver
+from taxon_names_resolver import taxTree
+
+# EXAMPLE NAMES
 terms = ['Homo sapiens', 'Gorilla gorilla', 'Pongo pongo', 'Macca mulatta',
          'Mus musculus', 'Ailuropoda melanoleuca', 'Ailurus fulgens',
          'Chlorotalpa tytonis', 'Arabidopsis thaliana', 'Bacillus subtilus']
+
+# RESOLVE
 resolver = Resolver(terms=terms, datasource="NCBI")
 resolver.main()
 
-from taxon_names_resolver import taxTree
+# CREATE TREE
 ranks = resolver.retrieve('classification_path_ranks')
 idents = resolver.retrieve('query_name')
 lineages = resolver.retrieve('classification_path')
-taxTree(idents, ranks, lineages, draw=True)
+treestring = taxTree(idents, ranks, lineages)
+
+# SAVE
+with open('example.tre', 'w') as file:
+    file.write(treestring)
